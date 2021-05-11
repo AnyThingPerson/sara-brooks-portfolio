1a50a20180babda34b1a6de3ada74618f4ea83f6
AnyThingPerson <66855064+AnyThingPerson@users.noreply.github.com>
2021-05-11 14:23:47 -0600

    Uploaded site files

---
+++ ml.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+<br>Your comments were <?php echo $_POST["comment"]; ?>
+
+
+</body>
+</html>
